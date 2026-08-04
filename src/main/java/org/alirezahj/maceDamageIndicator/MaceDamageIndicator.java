2bfd1ad7b52faab9fee6e7b4a6d347617282b1dc
alireza2012hj <alirezagamer455@gmail.com>
2026-08-04 13:37:04 +0330

    1st Commit

---
+++ src/main/java/org/alirezahj/maceDamageIndicator/MaceDamageIndicator.java
@@ -0,0 +1,16 @@
+package org.alirezahj.maceDamageIndicator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MaceDamageIndicator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("MaceDamageIndicator Enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("MaceDamageIndicator Disabled!");
+    }
+}
---
+++ src/main/java/org/alirezahj/maceDamageIndicator/MaceDamageIndicatorLoader.java
@@ -0,0 +1,12 @@
+package org.alirezahj.maceDamageIndicator;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+
+class MaceDamageIndicatorLoader implements PluginLoader {
+
+    @Override
+    public void classloader(final PluginClasspathBuilder builder) {
+        // Add dynamically loaded libraries here
+    }
+}
